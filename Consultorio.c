8f9bb136c7be326744e24fb03f648217fa7bd470
CeliaGonz <celia.gonzalez@alumnos.upm.es>
2021-04-29 20:56:16 +0200

    Estructuras
    He añadido una estructura de usuarios

--- Consultorio.c
+++ Consultorio.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 #define N 100
+#define MAX_USUARIO 100
+
+// Estructura Usuarios
+struct Usuario {
+	char nombre[N];
+	char primer_apellido[N];
+	char segundo_apellido[N];
+	char usuario[N];
+	char correo_electronico[N];
+	char contrasennya[N];
+};
 
 int main () {
 	
@@ -10,22 +22,28 @@ int main () {
 	printf("                                             CONSULTORIO CFL                        \n");
 	printf("                    ________________________________________________________________\n");
 	
-    printf("\n");		
+    printf("\n");	
+	
+	struct Usuario nuevo_usuario;
 	int opcion=0;
-	char nombre[N], primer_apellido[N], segundo_apellido[N], usuario[N], correo_electronico[N], contrasennya[N];
-	int dia, mes, annyo;
+	int num_usuarios=0;
+	char usuario[N], contrasennya[N];
+	
+	printf("Bienvenidos al cuestionario mas entretenido y con mayor variedad de opciones, asi descubriras para lo que estas hecho.\n");
 	
 	do {
 		
-	    printf("Bienvenidos al cuestionario mas entretenido y con mayor variedad de opciones, asi descubriras para lo que estas hecho.\n");
-	    printf("\n");
-	    printf("Elige una opcion para continuar:\n");
-	    printf("   1-Instrucciones.\n");
-	    printf("   2-Regitrarse.\n");
-	    printf("   3-Iniciar sesion para comenzar.\n");
-	    printf("   4-Salir.\n");
-	    scanf("%d", &opcion);
-
+		do {
+			// Menú de opciones
+	        printf("\n");
+	        printf("Elige una opcion para continuar:\n");
+	        printf("   1-Instrucciones.\n");
+	        printf("   2-Regitrarse.\n");
+	        printf("   3-Iniciar sesion para comenzar.\n");
+	        printf("   4-Salir.\n");
+	        scanf("%d", &opcion);
+		} while (opcion!=1 && opcion!=2 && opcion!=3 && opcion!=4);
+	   
 	    switch (opcion) {
 		    case 1: 
 		        printf("Instrucciones.\n");
@@ -37,55 +55,59 @@ int main () {
 			break;
 		        
 		    case 2:
+		    	// Registro nuevo usuario
 		    	printf("Registrarse.\n");
-			    printf("Nombre:\n");
-			    scanf("%s", nombre);
+		    	
+		    	
+		        printf("Nombre:\n");
+			    scanf("%s", nuevo_usuario.nombre);
 			
 			    printf("Primer apellido:\n");
 			    fflush(stdin);
-			    scanf("%s", primer_apellido);
+			    scanf("%s", nuevo_usuario.primer_apellido);
 			
 			    printf("Segundo apellido:\n");
 			    fflush(stdin);
-			    scanf("%s", segundo_apellido);
-			
-			    printf("Fecha de nacimiento (dia mes annyo)\n");
-			    fflush(stdin);
-			    scanf("%d %d %d", &dia, &mes, &annyo);
+			    scanf("%s", nuevo_usuario.segundo_apellido);
 			
 			    printf("Correo electronico\n");
 			    fflush(stdin);
-		        scanf("%s", correo_electronico);
+		        scanf("%s", nuevo_usuario.correo_electronico);
 		    
 		        printf("Nombre de usuario:\n");
 		        fflush(stdin);
-			    scanf("%s", usuario);		
+			    scanf("%s", nuevo_usuario.usuario);		
 			
 			    printf("Contrasennya:\n");
 			    fflush(stdin);
-			    scanf("%s", contrasennya);
+			    scanf("%s", nuevo_usuario.contrasennya);
 			
-			    printf("Bienvenido %s.\n", usuario);
-			    
+			    printf("Bienvenido %s.\n", nuevo_usuario.usuario);
+			        
+			    num_usuarios++;
+			        
+			    // printf("Has sido la persona numero %d en unirse.\n", num_usuarios); (con ficheros)
+			        
 			break;
 		  
 		    case 3: 
+		        // Inicio de sesion para un usuario ya registrado 
 		    	printf("Iniciar sesion para comenzar.\n");
 			    printf("Introduce el nombre de usuario:");
 			    scanf("%s", usuario);
 			    
 			    printf("Introduce la contrasennya:");
-			    scanf("%s",contrasennya);
-			    
+			    scanf("%s", contrasennya);
+			 
 			    printf("Bienvenido de nuevo %s", usuario);
 		
 			break;
-			    
+			
 		    case 4:
-			    printf("Salir.\n");
+		    	// Salida del programa
+			    printf("Saliendo...\n");
 			    
 			break;
-			    
 	    }
 	    
 	} while (opcion!=4);
