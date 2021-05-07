44c185aece3ddd5e33ad8e0d85679efb8a776691
CeliaGonz <celia.gonzalez@alumnos.upm.es>
2021-05-07 20:22:44 +0200

    Un cambio cuando se pide el nombre de usuario
    Ahora si el nombre de usuario introducido ya existe, se vuelve a pedir otro

--- Consultorio.c
+++ Consultorio.c
@@ -8,8 +8,8 @@ struct Usuario {
 	char nombre[N];
 	char primer_apellido[N];
 	char segundo_apellido[N];
+	char correo_electronico[N];	
 	char usuario[N];
-	char correo_electronico[N];
 	char contrasennya[N];
 };
 
@@ -24,26 +24,26 @@ int main () {
 	
     printf("\n");	
     
-	FILE* fichero;
+	FILE * fichero;
     struct Usuario usuarios[MAX_USUARIO];
 	struct Usuario nuevo_usuario;
-	int opcion=0, i;
-	int num_usuarios=0, sesion_iniciada=0;
+	int num_usuarios=0;
+	int opcion, i, sesion_iniciada;
 	char usuario[N], contrasennya[N];
 	
-	
-	 // CARGA DE DATOS
+	// CARGA DE DATOS
     fichero = fopen("usuarios.txt", "r");
     
     if(fichero == NULL) {
         printf("Error al cargar los datos\n");
+        return 0;
     }
-    else {
-        while (num_usuarios < MAX_USUARIO && fscanf(fichero, "%s %s %s %s %s %s", usuarios[num_usuarios].nombre, usuarios[num_usuarios].primer_apellido, usuarios[num_usuarios].segundo_apellido, usuarios[num_usuarios].usuario, usuarios[num_usuarios].correo_electronico, usuarios[num_usuarios].contrasennya) != EOF) {
-            num_usuarios++;
-        }
-        fclose(fichero);
+    
+    while (num_usuarios < MAX_USUARIO && fscanf(fichero, "%s %s %s %s %s %s", usuarios[num_usuarios].nombre, usuarios[num_usuarios].primer_apellido, usuarios[num_usuarios].segundo_apellido, usuarios[num_usuarios].correo_electronico, usuarios[num_usuarios].usuario, usuarios[num_usuarios].contrasennya) != EOF) {
+		num_usuarios++;
     }
+    
+    fclose(fichero);
     
 	printf("Bienvenidos al cuestionario mas entretenido y con mayor variedad de opciones, asi descubriras para lo que estas hecho.\n");
 	
@@ -74,81 +74,107 @@ int main () {
 		    	// Registro nuevo usuario
 		    	printf("Registrarse.\n");
 		    	
-		    	
-		        printf("Nombre:\n");
-			    scanf("%s", nuevo_usuario.nombre);
+		    	if (num_usuarios < MAX_USUARIO){
+		    		printf("Nombre:\n");
+			        scanf("%s", nuevo_usuario.nombre);
 			
-			    printf("Primer apellido:\n");
-			    fflush(stdin);
-			    scanf("%s", nuevo_usuario.primer_apellido);
+			        printf("Primer apellido:\n");
+			        fflush(stdin);
+			        scanf("%s", nuevo_usuario.primer_apellido);
+			    
+			        printf("Segundo apellido:\n");
+			        fflush(stdin);
+			        scanf("%s", nuevo_usuario.segundo_apellido);
 			
-			    printf("Segundo apellido:\n");
-			    fflush(stdin);
-			    scanf("%s", nuevo_usuario.segundo_apellido);
+			        printf("Correo electronico\n");
+			        fflush(stdin);
+		            scanf("%s", nuevo_usuario.correo_electronico);
+		            
+		            i=0;
+					do{
+			        	printf("Nombre de usuario:\n");
+			            fflush(stdin);
+				        scanf("%s", nuevo_usuario.usuario);	
+				        for (i=0; i<num_usuarios; i++){
+				        	if (strcmp(usuarios[i].usuario, nuevo_usuario.usuario)==0){
+				        		printf("Este nombre de usuario ya esta elegido.\nIntroduzca otro\n");
+				        		break;
+							}
+						}
+					} while (strcmp(usuarios[i].usuario, nuevo_usuario.usuario)==0 );
+					
+					if (strcmp(usuarios[i].usuario, nuevo_usuario.usuario)!=0){
+						strcpy(usuarios[num_usuarios].usuario, nuevo_usuario.usuario);
+					}
 			
-			    printf("Correo electronico\n");
-			    fflush(stdin);
-		        scanf("%s", nuevo_usuario.correo_electronico);
+			        printf("Contrasennya:\n");
+			        fflush(stdin);
+			        scanf("%s", nuevo_usuario.contrasennya);
+			
+			        printf("\nBienvenido %s.\n", nuevo_usuario.usuario);
+			        
+			        num_usuarios++;
+				    usuarios[num_usuarios - 1] = nuevo_usuario;
+				    printf("Has sido la persona numero %d en registrarse.\n", num_usuarios);
+				}
+				
+				else {
+					printf("Lo sentimos, numero maximo de usuarios alcanzado.\n");
+				}
 		    
-		        printf("Nombre de usuario:\n");
-		        fflush(stdin);
-			    scanf("%s", nuevo_usuario.usuario);		
-			
-			    printf("Contrasennya:\n");
-			    fflush(stdin);
-			    scanf("%s", nuevo_usuario.contrasennya);
-			
-			    printf("Bienvenido %s.\n", nuevo_usuario.usuario);
-			        
-			    num_usuarios++;
-			        
-			    printf("Has sido la persona numero %d en unirse.\n", num_usuarios); 
-			        
 			break;
-		  
+			
 		    case 3: 
 		        // Inicio de sesion para un usuario ya registrado 
 		    	printf("Iniciar sesion para comenzar.\n");
 			    printf("Introduce el nombre de usuario:");
+			    fflush(stdin);
 			    scanf("%s", usuario);
 			    
 			    printf("Introduce la contrasennya:");
+			    fflush(stdin);
 			    scanf("%s", contrasennya);
 			    
-			    for(i = 0; i < num_usuarios; i++) {
-			    	if(strncmp(usuario, usuarios[i].usuario, N) == 0 && strncmp(contrasennya, usuarios[i].contrasennya, N) == 0) {
+			    sesion_iniciada=0;
+			    for(i=0; i<num_usuarios; i++) {
+			    	if( (strcmp(usuario, usuarios[i].usuario)==0) && (strcmp(contrasennya, usuarios[i].contrasennya)==0) )   {
 			    		printf("\nBienvenido de nuevo %s\n", usuario);
-			    		sesion_iniciada = 1;
+			    		sesion_iniciada=1;
 					}
 				}
 				
-				if(sesion_iniciada) {
-					
-					
-					
-					// PREGUNTAS AQUI
-					
-					
+				if(sesion_iniciada==1) {
+					// PREGUNTAS AQUI	
+					printf("Primera Pregunta:\n");
 				}
-		
-			 
-			   	break;
+				
+				else if (sesion_iniciada==0){
+					printf("\nUsuario o contrasennya no validos\n");
+				}
+				
+			break;
 			
 		    case 4:
 		    	// Salida del programa
 			    printf("Saliendo...\n");
 			    
-			     fichero = fopen("usuarios.txt", "w");
-                
-                for(i = 0; i < num_usuarios; i++) {
-                    fprintf(fichero, "%s %s %s %s %s %s\n", usuarios[i].nombre, usuarios[i].primer_apellido, usuarios[i].segundo_apellido, usuarios[i].usuario, usuarios[i].correo_electronico, usuarios[i].contrasennya);
+	            fichero = fopen("usuarios.txt", "w");
+	
+                if (fichero == NULL) {
+		            printf("Error abriendo fichero para escritura.");
+		            return 0;
+	            }
+	
+                for(i=0; i<num_usuarios; i++) {
+                    fprintf(fichero, "%s %s %s %s %s %s\n", usuarios[i].nombre, usuarios[i].primer_apellido, usuarios[i].segundo_apellido, usuarios[i].correo_electronico, usuarios[i].usuario, usuarios[i].contrasennya);
                 }
+                
                 fclose(fichero);
-			    
+                
 			break;
 	    }
 	    
 	} while (opcion!=4);
-	 
+	
 	return 0;
 }
