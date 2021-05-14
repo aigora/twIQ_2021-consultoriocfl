ee34ca7a4f725047ae6e408930d8affd08ea0dd8
CeliaGonz <celia.gonzalez@alumnos.upm.es>
2021-05-14 22:57:37 +0200

    Mas preguntas
    He añadido las preguntas relacionadas con Ciencias Sociales y Humanidades.

--- Consultorio.c
+++ Consultorio.c
@@ -14,6 +14,7 @@ struct Usuario {
 };
 void imprimirMenu();
 int compararString(char cadena1[], char cadena2[]);
+int longitudString(char cadena[]);
 
 int main () {
 	
@@ -32,7 +33,7 @@ int main () {
 	int num_usuarios=0;
 	int opcion, i, sesion_iniciada;
 	char usuario[N], contrasennya[N];
-	char respuesta1,respuesta2;
+	char respuesta[2];
 	int opcion2;
 	
 	
@@ -56,7 +57,7 @@ int main () {
 		
 		do {
 			// Menú de opciones
-	     imprimirMenu();
+	        imprimirMenu();
 	        scanf("%d", &opcion);
 		} while (opcion!=1 && opcion!=2 && opcion!=3 && opcion!=4);
 	   
@@ -137,58 +138,168 @@ int main () {
 			    
 			    sesion_iniciada=0;
 			    for(i=0; i<num_usuarios; i++) {
-			    	if( (strcmp(usuario, usuarios[i].usuario)==0) && (strcmp(contrasennya, usuarios[i].contrasennya)==0) )   {
+			    	if( (compararString(usuario, usuarios[i].usuario)==0) && (compararString(contrasennya, usuarios[i].contrasennya)==0) )   {
 			    		printf("\nBienvenido de nuevo %s\n", usuario);
 			    		sesion_iniciada=1;
 					}
 				}
 				
 				if(sesion_iniciada==1) {
-					//Preguntas
+					//Preguntas 	
 					printf("¿Que modalidad estas estudiando?\n");
-					
 					printf("1.Ciencias\n2.Humanidades y Ciencias Sociales\n3.Artes\n");
-					scanf("%d",&opcion2);
+					scanf("%d", &opcion2);
 					
 					switch(opcion2){
 						case 1:
 							printf("¿Te desenvuelves bien con los ordenadores?(Si o No)\n");
-							scanf("%s",respuesta1);
-							if (respuesta1=='Si'|| respuesta1=='si'){
-								
-								
-							}
-							else if (respuesta1=='No'||respuesta1=='no') {
-									
-							}
-							
-							break;
-				
+							scanf("%s", respuesta);							
+						break;
 						case 2:
-							printf("¿Te sientes comodo cuando hablas en publico?(Si o No)\n");
-							scanf("%s",respuesta2);
-						    if (respuesta2=='Si'|| respuesta2=='si'){
-						    	printf("¿");
-						    	
-			
-								
+							printf("Te interesa conocer el pasado para asi enterder el presente?(Si o No)\n");
+							fflush(stdin);
+							scanf("%s", respuesta);
+							if (compararString(respuesta, "Si") == 0 || compararString(respuesta, "si") == 0){
+								printf("¿Te gusta leer? ¿Dedicas tiempo a lectura?\n¿Te gustan las novelas clasicas como EL Quijote, La Casa de Bernanda Alba? (SI/NO)\n");
+								fflush(stdin);
+								scanf("%s", respuesta);
+								if (compararString(respuesta, "Si") == 0 || compararString(respuesta, "si") == 0){
+									printf("¿Te gustaria aprender otro idioma con profundidad?(Si O No)\n");
+									fflush(stdin);
+									scanf("%s", respuesta);
+									if (compararString(respuesta, "Si") == 0 || compararString(respuesta, "si") == 0){
+										printf("Podrias estudiar: Estudios Ingleses, Estudios Semiticos e Islamicos, Filologia CLasica o Traduccion e interpretacion.\n");
+									}
+									else if (compararString(respuesta, "No") == 0 || compararString(respuesta, "no") == 0){
+										printf("Podrias estudiar: Español,lengua y literatura; Lenguas Modernas y sus literaturas o Literarura Gerenal y Comparada.\n");
+									}
+								}
+								else if (compararString(respuesta, "No") == 0 || compararString(respuesta, "no") == 0){
+									printf("¿Frecuentas museos historicos y/o arquelogicos?(Si o No)\n");
+									fflush(stdin);
+									scanf("%s", respuesta);
+									if (compararString(respuesta, "Si") == 0 || compararString(respuesta, "si") == 0){
+										printf("Podrias estudiar: Historia o Arqueologia.\n");
+									}
+									else if (compararString(respuesta, "No") == 0 || compararString(respuesta, "no") == 0){
+										printf("Podrias estudiar: Filosofia o Ciencias de las religiones.\n");
+									}
+								}
 							}
-							else if (respuesta2=='No'||respuesta2=='no') {
-									
+							else if (compararString(respuesta, "No") == 0 || compararString(respuesta, "no") == 0){
+								printf("¿Te consideras una persona creativa, imaginativa y curiosa?(Si o No)\n");
+								fflush(stdin);
+								scanf("%s", respuesta);
+								if(compararString(respuesta, "Si") == 0 || compararString(respuesta, "si") == 0){
+									printf("Podrias estudiar: Periodimo o Publicidas y Relaciones publicas.\n");
+								}
+								else if(compararString(respuesta, "No") == 0 || compararString(respuesta, "no") == 0){
+									printf("¿Te sientes comodo al hablar en publico?(Si o No)\n");
+									fflush(stdin);
+									scanf("%s", respuesta);
+									if (compararString(respuesta, "Si") == 0 || compararString(respuesta, "si") == 0){
+										printf("¿Te interesan los debates sobre temas de actualidad?(Si o No)\n");
+										fflush(stdin);
+										scanf("%s", respuesta);
+										if(compararString(respuesta, "Si") == 0 || compararString(respuesta, "si") == 0){
+											printf("Pordias estudiar: Ciencias politicas o Derecho.\n");
+										}
+										else if(compararString(respuesta, "No") == 0 || compararString(respuesta, "no") == 0){
+											printf("Podrias estudiar: Antropologia Social y Cultural, Magisterio Infantil, Magisterio en Eduacon Primaria(opcion en bilingue), Pedagogia, Sociologia o Trabajo Social.\n");
+										}
+									}
+									else if (compararString(respuesta, "No") == 0 || compararString(respuesta, "no") == 0){
+										printf("¿Te gustaria dirigir tu propia empresa?(Si o No)\n");
+	     							    fflush(stdin);
+										scanf("%s", respuesta);
+										if (compararString(respuesta, "Si") == 0 || compararString(respuesta, "si") == 0){
+											printf("¿Te manejas bien con cuentas matematicas?(Si o No)\n");
+											fflush(stdin);
+											scanf("%s", respuesta);
+											if (compararString(respuesta, "Si") == 0 || compararString(respuesta, "si") == 0){
+												printf("Podrias estudiar: Economia y finanzas.\n");
+											}
+											else if (compararString(respuesta, "No") == 0 || compararString(respuesta, "no") == 0){
+												printf("Podrias estudiar: Administracion y Direccion de Empresas (opcion en ingles) o Relaciones Internacionales\n");
+											}
+										}
+										else if (compararString(respuesta, "No") == 0 || compararString(respuesta, "no") == 0){
+											printf("Podrias estudiar: Comercio o turismo.\n");
+										}
+									}
+								}
 							}
-							
+						break;	
 						
-						
-							break;
-							
 						case 3:
-							break;
+							printf("¿Sueles acudir a teatros, operas, danzas, conservatorios?(Si o No)\n");
+							fflush(stdin);
+							scanf("%s",respuesta);
+							if(compararString(respuesta, "Si") == 0 || compararString(respuesta, "si") == 0) {
+								printf("¿Te apasiona bailar?(Si o No)\n");
+								fflush(stdin);
+								scanf("%s",respuesta);
+								if(compararString(respuesta, "Si") == 0 || compararString(respuesta, "si") == 0) {
+									printf("GRADO DE DANZA\n");
+								}
+								else if(compararString(respuesta, "No") == 0 || compararString(respuesta, "no") == 0) {
+									printf("¿Disfrutas componiendo obras y tocando instrumentos?(Si o No)\n");
+									fflush(stdin);
+									scanf("%s",respuesta);	
+									if(compararString(respuesta, "Si") == 0 || compararString(respuesta, "si") == 0) {
+										printf("GRADO EN MUSICOLOGIA\n");
+									}
+									else if(compararString(respuesta, "No") == 0 || compararString(respuesta, "no") == 0) {
+										printf("¿Te interesa la evolucion artistica y cultural del ser humano?(Si o No)\n");
+										fflush(stdin);
+										scanf("%s",respuesta);
+										if(compararString(respuesta, "Si") == 0 || compararString(respuesta, "si") == 0) {
+											printf("HISTORIA DEL ARTE\n");
+										}
+										else if(compararString(respuesta, "No") == 0 || compararString(respuesta, "no") == 0) {
+											printf("¿Tienes la capacidad de la creacion y analisis de las artes visuales?(Si o No)\n");
+											fflush(stdin);
+											scanf("%s",respuesta);	
+											if(compararString(respuesta, "Si") == 0 || compararString(respuesta, "si") == 0) {
+												printf("BELLAS ARTES\n");
+											}
+											else if(compararString(respuesta, "No") == 0 || compararString(respuesta, "no") == 0) {
+												printf("ARTES ESCENICAS\n");	
+											}
+										}		
+									}
+								}
+							}
+							else if(compararString(respuesta, "No") == 0 || compararString(respuesta, "no") == 0) {
+								printf("¿Eres capaz de trasmitir y representar valores a traves de diversos modos de comunicacion?(Si o No)\n");
+								fflush(stdin);
+								scanf("%s",respuesta);	
+								if(compararString(respuesta, "Si") == 0 || compararString(respuesta, "si") == 0) {
+									printf("DISEÑO GRAFICO\n");
+								}
+								else if(compararString(respuesta, "No") == 0 || compararString(respuesta, "no") == 0) {
+									printf("¿Dedicas tu tiempo libre a la moda?(Si o No)\n");
+									fflush(stdin);
+									scanf("%s",respuesta);
+									if(compararString(respuesta, "Si") == 0 || compararString(respuesta, "si") == 0) {
+										printf("DISEÑO DE MODA\n");
+									}
+									else if(compararString(respuesta, "No") == 0 || compararString(respuesta, "no") == 0) {
+										printf("¿Tu sueño es crear arte con la tecnologia?(Si o No)\n");
+										fflush(stdin);
+										scanf("%s",respuesta);	
+										if(compararString(respuesta, "Si") == 0 || compararString(respuesta, "si") == 0) {
+											printf("DISEÑO DIGITAL\n");
+										}
+										else if(compararString(respuesta, "No") == 0 || compararString(respuesta, "no") == 0) {
+											printf("DISEÑO DE INTERIORES\n");	
+										}
+									}		
+								}	
+							}
+						break;	
 					}
-					
-					
-				
 				}
-				
 				else if (sesion_iniciada==0){
 					printf("\nUsuario o contrasennya no validos\n");
 				}
@@ -222,25 +333,35 @@ int main () {
 
 void imprimirMenu(){
 	printf("\n");
- printf("Elige una opcion para continuar:\n");
-	        printf("   1-Instrucciones.\n");
-	        printf("   2-Regitrarse.\n");
-	        printf("   3-Iniciar sesion para comenzar.\n");
-	        printf("   4-Salir.\n");
-	
-}
-
+	printf("Elige una opcion para continuar:\n");
+	printf("   1-Instrucciones.\n");
+	printf("   2-Regitrarse.\n");
+	printf("   3-Iniciar sesion para comenzar.\n");
+	printf("   4-Salir.\n");
+}   
 
 int compararString(char cadena1[], char cadena2[]){
-	int i=0;
-	while(cadena1[i] != '0' && cadena2[i] != '0') {
-		if(cadena1[i]==cadena2[i]){
-			return 0;
-		}
-		
-		else if(cadena1[i]!=cadena2[i]){
-			return 1;
-		}
-		i++;
+	int resul = 0, i;
+	
+	if(longitudString(cadena1) != longitudString(cadena2)){
+		return 1;
 	}
+	
+	for(i = 0; i < longitudString(cadena1); i++){
+		if(cadena1[i] != cadena2[i]){
+			resul = 1;
+		}
+	}
+	
+	return resul;
+}
+
+int longitudString(char cadena[]) {
+	int longitud = 0;
+	
+	while(cadena[longitud] != '\0') {
+		longitud++;
+	}
+	
+	return longitud;
 }
