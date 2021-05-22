9636e78153de846b183d67a5c7b9e12ed713bcea
luciaalmendros <lucia.almendros@alumnos.upm.es>
2021-05-22 18:50:29 +0200

    Update Consultorio.c

--- Consultorio.c
+++ Consultorio.c
@@ -373,7 +373,8 @@ int main () {
 			
 		    case 4:
 		    	// Salida del programa
-			    printf("Saliendo...\n");
+			    printf("Esperamos que te haya sido de gran ayuda este cuestionario.\n");
+			    printf("Recuerda que la decisión final es la tuya, la mas importante. Y si no te gusta, puedes probar otra cosa!.\n");
 			    
 	            fichero = fopen("usuarios.txt", "w");
 	
